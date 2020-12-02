546b713479b7597e5539ffce6b969ec4bf9c6c3b
kotasugya <no.shaking.throne974zero@gmail.com>
2020-12-02 21:21:54 +0900

    Finish Janken

--- janken.rb
+++ janken.rb
@@ -2,23 +2,54 @@
 def janken
   puts "じゃんけん..."
   jankens = ["グー", "チョキ", "パー"]
-  puts "0(グー) 1(チョキ) 2(パー) 3(しっぽを巻いて逃げ出す)"
+  puts "0(グー) 1(チョキ) 2(パー)"
   @your_hand = gets.to_i
-  @enemy_hand = rand(4)
+  @enemy_hand = rand(3)
   puts "-----------------"
   puts "あなたは#{jankens[@your_hand]}を出しました"
   puts "相手は#{jankens[@enemy_hand]}を出しました"
   puts "-----------------"
+  
+  if @your_hand == @enemy_hand
+    puts "あいこです"
+    return true
+  elsif (@your_hand == 0 && @enemy_hand == 2) || 
+    (@your_hand == 1 && @enemy_hand == 0) ||
+    (@your_hand == 2 && @enemy_hand == 1)
+    # hoi  #あっち向いてホイ
+    # hoi_loss
+    puts "あなたの負けです"
+    return false
+    
+  elsif @your_hand >= 3
+    puts "そんな選択肢はありません、選び直してください"
+    return true
+  else
+    puts "あなたの勝ちです"
+    # hoi
+    # hoi_win
+    return false
+  end
 end
+
+janken
+
+next_game = true
+
+while next_game do
+  next_game = janken
+end
+
+------------------------------
 #あっち向いてホイ
 def hoi
   hois = ["上", "下", "左", "右"]
   puts "あっち向いて"
   puts "0(上) 1(下) 2(左) 3(右)"
   @your_dir = gets.to_i
-  @enemy_dir = rand(5)
-  puts "ホイ"
+  @enemy_dir = rand(4)
   puts "-----------------"
+  puts "ホイ"
   puts "あなた: #{hois[@your_dir]}"
   puts "相手: #{hois[@enemy_dir]}"
   puts "-----------------"
@@ -28,7 +59,7 @@ end
 def hoi_win
   if @your_dir == @enemy_dir
     puts "あなたの勝ちです"
-  else #それ以外はじゃんけんに戻す
+  else#それ以外はじゃんけんに戻す
     janken
   end
 end
@@ -39,29 +70,6 @@ def hoi_loss
   else　#それ以外はじゃんけんに戻す
     janken
   end
-end
-
-
-janken
-
-
-#じゃんけん判定とあっち向いてホイの結果
-if @your_hand == @enemy_hand
-  puts "あいこです"
-  janken
-
-elsif #じゃんけんで負けた場合
-  (@your_hand == 0 && @enemy_hand == 2) || 
-  (@your_hand == 1 && @enemy_hand == 0) ||
-  (@your_hand == 2 && @enemy_hand == 1)
-  hoi
-  hoi_loss
-
-elsif @your_hand == 3
-  puts "あなたは全てに敗北しました"
-
-else　#勝った場合
-  puts "あなたの勝ちです"
 end
 
 

19aa5b2d11d2a1ba9839f166c6d42b5ed4730b67
kotasugya <no.shaking.throne974zero@gmail.com>
2020-12-03 00:30:14 +0900

    Finish janken_app

--- janken.rb
+++ janken.rb
@@ -1,3 +1,39 @@
+#あっち向いてホイ
+def hoi
+  hois = ["上", "下", "左", "右"]
+  puts "あっち向いて"
+  puts "0(上) 1(下) 2(左) 3(右)"
+  @your_dir = gets.to_i
+  @enemy_dir = rand(4)
+  puts "-----------------"
+  puts "ホイ"
+  puts "あなた: #{hois[@your_dir]}"
+  puts "相手: #{hois[@enemy_dir]}"
+  puts "-----------------"
+end
+
+def hoi_win
+  hoi
+  if @your_dir == @enemy_dir
+    puts "あなたの勝ちです"
+    return false
+  else#それ以外はじゃんけんに戻す
+    return true
+  end
+end
+
+def hoi_loss
+  hoi
+  if @enemy_dir == @your_dir
+    puts "あなたの負けです"
+    return false
+  else　#それ以外はじゃんけんに戻す
+    return true
+  end
+end
+
+next_game = true
+
 #じゃんけん
 
 def janken
@@ -18,20 +54,14 @@ def janken
   elsif (@your_hand == 0 && @enemy_hand == 2) || 
     (@your_hand == 1 && @enemy_hand == 0) ||
     (@your_hand == 2 && @enemy_hand == 1)
-    # hoi  #あっち向いてホイ
-    # hoi_loss
-    puts "あなたの負けです"
-    return false
+    hoi_loss
     
   elsif @your_hand >= 3
     puts "そんな選択肢はありません、選び直してください"
     return true
 
   else
-    puts "あなたの勝ちです"
-    # hoi
-    # hoi_win
-    return false
+    hoi_win
   end
 end
 
@@ -43,36 +73,5 @@ while next_game do
   next_game = janken
 end
 
-#------------------------------
-# #あっち向いてホイ
-# def hoi
-#   hois = ["上", "下", "左", "右"]
-#   puts "あっち向いて"
-#   puts "0(上) 1(下) 2(左) 3(右)"
-#   @your_dir = gets.to_i
-#   @enemy_dir = rand(4)
-#   puts "-----------------"
-#   puts "ホイ"
-#   puts "あなた: #{hois[@your_dir]}"
-#   puts "相手: #{hois[@enemy_dir]}"
-#   puts "-----------------"
-# end
-
-# #あっち向いてホイの判定
-# def hoi_win
-#   if @your_dir == @enemy_dir
-#     puts "あなたの勝ちです"
-#   else#それ以外はじゃんけんに戻す
-#     janken
-#   end
-# end
-
-# def hoi_loss
-#   if @enemy_dir == @your_dir
-#     puts "あなたの負けです"
-#   else　#それ以外はじゃんけんに戻す
-#     janken
-#   end
-# end
 
 
